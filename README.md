1bb0b9a5ba79dc5ebeba70c4f416381b4d9bcb79
zbx-sadman <sadman41@ya.ru>
2016-02-29 13:00:39 +0300

    USB HASP 1.0 uploaded

--- README.md
+++ README.md
@@ -1,43 +1,83 @@
-## NetHASP
+## NetHASP 
 This is a little Powershell script that fetch metric's values from NetHASP Monitor.
 Tanx to _Tor_ user for [HaspMonitor.exe](https://www.zabbix.com/forum/showpost.php?p=96243&postcount=4) utility.
 
+***REWORKED***
+
 Support objects:
 - _Server_ - NetHASP server that can detected with "GET SERVERS" command;
-- _Slot_ - NetHASP Key Slot that can detected with "GET SLOTS ..." command;
+- _Slot_ - NetHASP Key Slot that can detected with "GET SLOTS ..." command.
+
+Actions:
+- _Discovery_ - Make Zabbix's LLD JSON;
+- _Get_       - Get metric of object collection item;
+- _Count_     - Count collection items.
 
 Zabbix's LLD available to:
-- _Server_ - A little fastest that Slot's LLD, but have few Zabbix's Macros
-- _Slot_ - Slowly, but more usable than Server's LLD, because have linked {#SERVERNAME}, {#SERVERID} & {#MAX} (max available licenses on slot);
+- _Server_ - A little fastest that Slot's LLD, but have few Zabbix's Macros;
+- _Slot_ - Slowly, but more usable than Server's LLD, because have linked {#SERVERNAME}, {#SERVERID} & {#MAX} (max available licenses on slot).
 
-How to use:
-- Just add to Zabbix Agent config, which run on any host, that can find NeHASP servers, that string: _UserParameter=nethasp[*], powershell -File C:\zabbix\scripts\nethasp\nethasp.ps1 -Action "$1" -Object "$2" -Key "$3" -Id "$4" -Slot "$5"_;
-- Put _nethasp.ps1, HaspMonitor.exe, hsmon.dll, nethasp.ini_ to _C:\zabbix\scripts\nethasp_ dir;
-- Change NH_SERVER_ADDR into _nethasp.ini_ to yours NetHASP server or enable Broadcast feature;
-- Make unsigned .ps1 script executable with _Set-ExecutionPolicy RemoteSigned_;
-- Set Zabbix Agent's / Server's _Timeout_ to more that 3 sec (may be 10 or 30);
-- Import [template](https://github.com/zbx-sadman/HASP/tree/master/Zabbix_Templates) to Zabbix Server;
-- Enjoy.
+###How to use standalone
+    # Make Zabbix's LLD JSON for HASP Key Servers
+    powershell -NoProfile -ExecutionPolicy "RemoteSigned" -File "nethasp.ps1" -Action "Discovery" -Object "Server"
 
-Hints:
+    # Return number of HASP keys
+    ... usbhasp.ps1 -Action "Get" -Object "Slot" -Slot 17 -Key Curr -Id "1CServ.admnet.local"
+
+    ...
+
+###How to use with Zabbix
+1. Just add to Zabbix Agent config, which run on any host, that can find NeHASP servers, that string: _UserParameter=nethasp[*], powershell -File C:\zabbix\scripts\nethasp\nethasp.ps1 -Action "$1" -Object "$2" -Key "$3" -Id "$4" -Slot "$5"_;
+2. Put _nethasp.ps1, HaspMonitor.exe, hsmon.dll, nethasp.ini_ to _C:\zabbix\scripts\nethasp_ dir;
+3. Change NH_SERVER_ADDR into _nethasp.ini_ to yours NetHASP server or enable Broadcast feature;
+4. Make unsigned .ps1 script executable with _Set-ExecutionPolicy RemoteSigned_;
+5. Set Zabbix Agent's / Server's _Timeout_ to more that 3 sec (may be 10 or 30);
+6. Import [template](https://github.com/zbx-sadman/HASP/tree/master/Zabbix_Templates) to Zabbix Server;
+7. Enjoy.
+
+**Note**
+Do not try import Zabbix v2.4 template to Zabbix _pre_ v2.4. You need to edit .xml file and make some changes at discovery_rule - filter tags area and change _#_ to _<>_ in trigger expressions. I will try to make template to old Zabbix.
+
+###Hints
 - All HaspMonitor answers are parsed to hash and available to read with **-Key** option. For example: Result of "GET SERVERINFO,ID=.." request - _HS,ID=1898799265,NAME="StuffSever",PROT="UDP(172.16.0.10)",VER="8.310",OS="WIN32"_  - will be parsed to hash array and can be addressed: _-Key OS_ will return _WIN32_;
 - NetHASP server can periodically change Server ID. And you can use **-Id** option with detected with "GET SERVERS" command server name: _-Id "StuffSever"_. Miner detect that name is specified do "GET SERVERS" command, take ID and use it with other operation.
 
 
 ## USBHASP
-The same that NetHASP Miner, but used for monitoring Sentinel/Aladdin HASP USB keys, which installed locally or binded with USB2IP.
+The same that NetHASP Miner, but used for monitoring Sentinel/Aladdin HASP USB keys, which installed locally or binded with USB/IP.
+
+Actual release 1.0
+
+Tested on Windows Server 2008R2 SP1, USB/IP service, Powershell 2.0
 
 Support objects:
-- _USBController_ - "physical" HASP keys (Win32_USBControllerDevice.Antecedent);
-- _LogicalDevice_ - "logical" HASP keys (Win32_USBControllerDevice.Dependent);
+- _USBController_ - "Physical" devices (USB Key, Win32_USBControllerDevice.Antecedent)
+- _LogicalDevice_ - "Logical" devices (HASP Key, Win32_USBControllerDevice.Dependent)
 
-How to use:
-- Put to Zabbix Agent config, which run on host where located HASP USB keys, that string: _UserParameter=usbhasp[*], powershell -File C:\zabbix\scripts\usbhasp.ps1 -Action "$1" -Object "$2" -Key "$3" -Id "$4"_;
-- Move _usbhasp.ps1_ to _C:\zabbix\scripts_ dir;
-- Make unsigned .ps1 script executable with _Set-ExecutionPolicy RemoteSigned_;
-- Set Zabbix Agent's / Server's _Timeout_ to more that 3 sec (may be 10 or 30);
-- Import [template](https://github.com/zbx-sadman/HASP/tree/master/Zabbix_Templates) to Zabbix Server;
-- Enjoy again.
+Actions:
+- _Discovery_ - Make Zabbix's LLD JSON;
+- _Get_       - Get metric of object collection item;
+- _Count_     - Count collection items.
+
+###How to use standalone
+
+    # Make Zabbix's LLD JSON for USB keys
+    powershell -NoProfile -ExecutionPolicy "RemoteSigned" -File "usbhasp.ps1" -Action "Discovery" -Object "USBController"
+
+    # Return number of HASP keys
+    ... usbhasp.ps1 -Action "Count" -Object "LogicalDevice"
+
+    # Show formatted list of 'USBController' object metrics selected by PnPId "USB\VID_0529&PID_0001\1&79F5D87&0&01". 
+    # Verbose messages is enabled. Note that PNPDeviceID is unique for USB Key, Id - is not.
+    ... usbhasp.ps1 -Action "Get" -Object "USBController" -PnPDeviceID "USB\VID_0529&PID_0001\1&79F5D87&0&01" -defaultConsoleWidth -Verbose
+
+###How to use with Zabbix
+1. Make setting to make unsigned .ps1 scripts executable for all time with _powershell.exe -command "Set-ExecutionPolicy RemoteSigned"_ or for once with _-ExecutionPolicy_ command line option;
+2. Just include [zbx\_hasp.conf](https://github.com/zbx-sadman/HASP/tree/master/Zabbix_Templates/zbx_hasp.conf) to Zabbix Agent config;
+3. Move _usbhasp.ps1_ to _C:\zabbix\scripts_ dir;
+4. Set Zabbix Agent's / Server's _Timeout_ to more that 3 sec (may be 10 or 30);
+5. Import [template](https://github.com/zbx-sadman/HASP/tree/master/Zabbix_Templates) to Zabbix Server;
+6. Enjoy again.
  
-Hints:
+###Hints
 - Be sure that you filter LLD to leave only 'HASP' or 'ALADDIN' records.
--- usbhasp/usbhasp.ps1
+++ usbhasp/usbhasp.ps1
@@ -1,100 +1,275 @@
 <#
+    .SYNOPSIS  
+        Return USB (HASP) Device metrics value, count selected objects, make LLD-JSON for Zabbix
 
-USB HASP Keys Miner
-Version 0.9
+    .DESCRIPTION
+        Return USB (HASP) Device metrics value, count selected objects, make LLD-JSON for Zabbix
 
-Return USB HASP key metric values, make LLD-JSON for Zabbix
+    .NOTES  
+        Version: 1.0
+        Name: USB HASP Keys Miner
+        Author: zbx.sadman@gmail.com
+        DateCreated: 29FEB2016
+        Testing environment: Windows Server 2008R2 SP1, USB/IP service, Powershell 2.0
 
-zbx.sadman@gmail.com (c) 2016
-https://github.com/zbx-sadman
+    .LINK  
+        https://github.com/zbx-sadman
 
-Actions: Discovery, Get
-Objects: USBController, LogicalDevice
-Id: Win32_USBControllerDevice.{Antecedent | Dependent}.PNPDeviceID
+    .PARAMETER Action
+        What need to do with collection or its item:
+            Discovery - Make Zabbix's LLD JSON;
+            Get       - Get metric from collection item;
+            Count     - Count collection items.
 
+    .PARAMETER Object
+        Define rule to make collection:
+            USBController - "Physical" devices (USB Key)
+            LogicalDevice - "Logical" devices (HASP Key)
 
+    .PARAMETER Key
+        Define "path" to collection item's metric 
+
+    .PARAMETER PnPDeviceID
+        Used to select only one item from collection
+
+    .PARAMETER ConsoleCP
+        Codepage of Windows console. Need to properly convert output to UTF-8
+
+    .PARAMETER DefaultConsoleWidth
+        Say to leave default console width and not grow its to $CONSOLE_WIDTH
+
+    .PARAMETER Verbose
+        Enable verbose messages
+
+    .EXAMPLE 
+        usbhasp.ps1 -Action "Discovery" -Object "USBController"
+
+        Description
+        -----------  
+        Make Zabbix's LLD JSON for USB keys
+
+    .EXAMPLE 
+        usbhasp.ps1 -Action "Count" -Object "LogicalDevice"
+
+        Description
+        -----------  
+        Return number of HASP keys
+
+    .EXAMPLE 
+        usbhasp.ps1 -Action "Get" -Object "USBController" -PnPDeviceID "USB\VID_0529&PID_0001\1&79F5D87&0&01" -defaultConsoleWidth -Verbose
+
+        Description
+        -----------  
+        Show formatted list of 'USBController' object metrics selected by PnPId "USB\VID_0529&PID_0001\1&79F5D87&0&01". Verbose messages is enabled
+        Note that PNPDeviceID is unique for USB Key, Id - is not.
 #>
 
 Param (
-[string]$Action,
-[string]$Object,
-[string]$Key,
-[string]$Id,
-[switch]$defaultConsoleWidth
-)
+        [Parameter(Mandatory = $True)] 
+        [string]$Action,
+        [Parameter(Mandatory = $True)]
+        [string]$Object,
+        [Parameter(Mandatory = $False)]
+        [string]$Key,
+        [Parameter(Mandatory = $False)]
+        [string]$PnPDeviceID,
+        [Parameter(Mandatory = $False)]
+        [string]$ConsoleCP,
+        [Parameter(Mandatory = $False)]
+        [switch]$DefaultConsoleWidth
+      )
 
+# Set US locale to properly formatting float numbers while converting to string
+[System.Threading.Thread]::CurrentThread.CurrentCulture = "en-US"
 
-Function Prepare-IDToLLD {
-  Param ([PSObject]$InObject);
+# Width of console to stop breaking JSON lines
+Set-Variable -Name "CONSOLE_WIDTH" -Value 255 -Option Constant -Scope Global
+
+####################################################################################################################################
+#
+#                                                  Function block
+#    
+####################################################################################################################################
+#
+#  Select object with ID if its given or with Any ID in another case
+#
+filter PnPDeviceIDEqualOrAny($PnPDeviceID) { if (($_.PNPDeviceID -Eq $PnPDeviceID) -Or (!$PnPDeviceID)) { $_ } }
+
+#
+#  Prepare string to using with Zabbix 
+#
+Function Prepare-ToZabbix {
+  Param (
+     [Parameter(Mandatory = $true, ValueFromPipeline = $true)] 
+     [PSObject]$InObject
+  );
   $InObject = ($InObject.ToString());
   $InObject.Replace("\", "\\");
+  $InObject.Replace("`"", "\`"");
 }
 
-Function Make-JSON {
-  Param ([PSObject]$InObject, [array]$ObjectProperties, [switch]$Pretty);
-  # Pretty json contain spaces, tabs and new-lines
-  if ($Pretty) { $CRLF = "`n"; $Tab = "    "; $Space = " "; } else {$CRLF = $Tab = $Space = "";}
-  # Init JSON-string $InObject
-  $Result += "{$CRLF$Space`"data`":[$CRLF";
-  # Take each Item from $InObject, get Properties that equal $ObjectProperties items and make JSON from its
-  $itFirstObject = $True;
-  ForEach ($Object in $InObject) {
-     if (-Not $itFirstObject) { $Result += ",$CRLF"; }
-     $itFirstObject=$False;
-     $Result += "$Tab$Tab{$Space"; 
-     $itFirstProperty = $True;
-     # Process properties. No comma printed after last item
-     ForEach ($Property in $ObjectProperties) {
-        if (-Not $itFirstProperty) { $Result += ",$Space" }
-        $itFirstProperty = $False;
-        $Result += "`"{#$Property}`":$Space`"$($Object.$Property)`""
-     }
-     # No comma printed after last string
-     $Result += "$Space}";
-  }
-  # Finalize and return JSON
-  "$Result$CRLF$Tab]$CRLF}";
-}
-
-# if needProcess is False - $Result is not need to convert to string and etc 
-$needProcess = $True;
-$needAction = $True;
-
-$ConnectedUSBDevices = Get-WmiObject Win32_USBControllerDevice;
-$ObjectProperties = @("NAME", "PNPDEVICEID");
-
-switch ($Object) {
-   'LogicalDevice' { $ConnectedUSBDevices = $ConnectedUSBDevices | % { [Wmi]$_.Dependent}; }
-   'USBController' { $ConnectedUSBDevices = $ConnectedUSBDevices | % { [Wmi]$_.Antecedent } | Get-Unique; }
-   default  { $Result = "Incorrect object: '$Object'"; $needAction = $False; }
-}
-
-if ($needAction) {
-   switch ($Action) {
-       #
-       # Discovery given object, make json for zabbix
-       #
-       'Discovery' {
-           $needProcess = $False;
-           $ConnectedUSBDevices | % { $_.PNPDeviceID = Prepare-IDToLLD($_.PNPDeviceID) };
-           $Result = Make-JSON -InObject $ConnectedUSBDevices -ObjectProperties $ObjectProperties -Pretty;
-       }
-       #
-       # Get metrics from object (real or virtual)
-       #
-       'Get' {
-          $Result = $ConnectedUSBDevices | ? { $_.PNPDeviceID -eq $Id };
-          if ($needProcess -And $Key) { $Result = $Result.$Key.ToString(); }
-       }
-       #
-       # Error
-       #
-       default  { $Result = "Incorrect action: '$Action'"; }
+#
+#  Convert incoming object's content to UTF-8
+#
+function ConvertTo-Encoding ([string]$From, [string]$To){  
+   Begin   {  
+      $encFrom = [System.Text.Encoding]::GetEncoding($from)  
+      $encTo = [System.Text.Encoding]::GetEncoding($to)  
+   }  
+   Process {  
+      $bytes = $encTo.GetBytes($_)  
+      $bytes = [System.Text.Encoding]::Convert($encFrom, $encTo, $bytes)  
+      $encTo.GetString($bytes)  
    }  
 }
-# Break lines on console output fix - buffer format to 255 chars width lines 
-if (!$defaultConsoleWidth) { mode con cols=255 }
+
+#
+#  Make & return JSON, due PoSh 2.0 haven't Covert-ToJSON
+#
+Function Make-JSON {
+   Param (
+      [Parameter(Mandatory = $true, ValueFromPipeline = $true)] 
+      [PSObject]$InObject, 
+      [array]$ObjectProperties, 
+      [switch]$Pretty
+   ); 
+   Begin   {
+               # Pretty json contain spaces, tabs and new-lines
+               if ($Pretty) { $CRLF = "`n"; $Tab = "    "; $Space = " "; } else {$CRLF = $Tab = $Space = "";}
+               # Init JSON-string $InObject
+               $Result += "{$CRLF$Space`"data`":[$CRLF";
+               # Take each Item from $InObject, get Properties that equal $ObjectProperties items and make JSON from its
+               $itFirstObject = $True;
+           } 
+   Process {
+               ForEach ($Object in $InObject) {
+                  if (-Not $itFirstObject) { $Result += ",$CRLF"; }
+                  $itFirstObject=$False;
+                  $Result += "$Tab$Tab{$Space"; 
+                  $itFirstProperty = $True;
+                  # Process properties. No comma printed after last item
+                  ForEach ($Property in $ObjectProperties) {
+                     if (-Not $itFirstProperty) { $Result += ",$Space" }
+                     $itFirstProperty = $False;
+                     $Result += "`"{#$Property}`":$Space`"$($Object.$Property | Prepare-ToZabbix)`""
+                  }
+                  # No comma printed after last string
+                  $Result += "$Space}";
+               }
+           }
+  End      {
+               # Finalize and return JSON
+               "$Result$CRLF$Tab]$CRLF}";
+           }
+}
+
+#
+#  Return value of object's metric defined by key-chain from $Keys Array
+#
+Function Get-Metric { 
+   Param (
+      [Parameter(Mandatory = $true, ValueFromPipeline = $true)] 
+      [PSObject]$InObject, 
+      [array]$Keys
+   ); 
+   # Expand all metrics related to keys contained in array step by step
+   $Keys | % { if ($_) { $InObject = $InObject | Select -Expand $_ }};
+   $InObject;
+}
+
+####################################################################################################################################
+#
+#                                                 Main code block
+#    
+####################################################################################################################################
+
+# split key
+$Keys = $Key.split(".");
+
+Write-Verbose "$(Get-Date) Taking Win32_USBControllerDevice with WMI"
+$Objects = Get-WmiObject Win32_USBControllerDevice;
+if (!$Objects)
+   {
+     Write-Warning "$(Get-Date) No devices found";
+     Exit;
+   }
+
+Write-Verbose "$(Get-Date) Creating collection of specified object: '$Object'";
+switch ($Object) {
+   'LogicalDevice' { 
+      $Objects = $Objects | % { [Wmi]$_.Dependent}; 
+   }
+   'USBController' { 
+      # Need to take Unique items due Senintel used multiply logical devices linked to physical keys. 
+      # As a result - double "physical" device items into 'Antecedent' branch
+      $Objects = $Objects | % { [Wmi]$_.Antecedent } | Get-Unique; 
+   }
+   default         {
+      Write-Error "Unknown object: '$Object'";
+      Exit;
+   }
+}
+
+$Objects = $Objects | PnPDeviceIDEqualOrAny $PnPDeviceID
+if (!$Objects)
+   {
+     Write-Error "$(Get-Date) No devices selected";
+     Exit;
+   }
+ 
+Write-Verbose "$(Get-Date) Processing collection with action: '$Action' ";
+switch ($Action) {
+   # Discovery given object, make json for zabbix
+  'Discovery' {
+      Write-Verbose "$(Get-Date) Generating LLD JSON";
+      $ObjectProperties = @("NAME", "PNPDEVICEID");
+      $Objects | % { $_.PNPDeviceID = $_.PNPDeviceID | Prepare-ToZabbix };
+      $Result = $Objects | Make-JSON -ObjectProperties $ObjectProperties -Pretty;
+  }
+  # Get metrics or metric list
+  'Get' {
+     if ($Keys) { 
+        Write-Verbose "$(Get-Date) Getting metric related to key: '$Key'";
+        $Result = $Objects | Get-Metric -Keys $Keys;
+     } else { 
+        Write-Verbose "$(Get-Date) Getting metric list due metric's Key not specified";
+        $Result = $Objects | fl *;
+     };
+  }
+  # Count selected objects
+  'Count' { 
+     Write-Verbose "$(Get-Date) Counting objects";  
+     # if result not null, False or 0 - return .Count
+     $Result = $(if ($Objects) { @($Objects).Count } else { 0 } ); 
+  }
+  default  { 
+     Write-Error "Unknown action: '$Action'";
+     Exit;
+  }  
+}
+
+Write-Verbose "$(Get-Date) Converting Windows DataTypes to equal Unix's / Zabbix's";
+switch (($Result.GetType()).Name) {
+   'Boolean'  { $Result = [int]$Result; }
+   'Object[]' { $Result = $Result | Out-String; }
+}
 
 # Normalize String object
-($Result | Out-String).trim();
+$Result = $Result.ToString().Trim();
+
+# Convert string to UTF-8 if need (For Zabbix LLD-JSON with Cyrillic chars for example)
+if ($consoleCP) { 
+   Write-Verbose "$(Get-Date) Converting output data to UTF-8";
+   $Result = $Result | ConvertTo-Encoding -From $consoleCP -To UTF-8; 
+}
+
+# Break lines on console output fix - buffer format to 255 chars width lines 
+if (!$defaultConsoleWidth) { 
+   Write-Verbose "$(Get-Date) Changing console width to $CONSOLE_WIDTH";
+   mode con cols=$CONSOLE_WIDTH; 
+}
+
+Write-Verbose "$(Get-Date) Finishing";
+
+"$Result";
 
